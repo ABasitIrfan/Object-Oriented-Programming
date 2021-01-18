2dbb501bc7607fe9bc89ab4d3f4beaccfcc3939c
Abdul Basit Irfan <47052299+ABasitIrfan@users.noreply.github.com>
2021-01-18 17:11:13 +0500

    Add files via upload

---
+++ DLL's/myDLL/myDLL/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myDLL
+{
+    public class Calculator
+    {
+        public int add(int a,int b)
+        {
+            return a + b;
+
+        }
+    }
+}
