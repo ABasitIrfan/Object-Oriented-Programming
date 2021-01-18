2cef419b1c356d28739fa60e1bb6b5aaa9b98b26
Abdul Basit Irfan <47052299+ABasitIrfan@users.noreply.github.com>
2021-01-18 17:07:35 +0500

    Add files via upload

---
+++ Calculator/ConsoleApplication1/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Calculator
+    {
+        public void add()
+        {
+
+        }
+        public void add(int a,int b)
+        {
+
+        }
+        public void add(float a,int b)
+        {
+
+        }
+        public void add(int a, float b)
+        {
+
+        }
+        //public void add( a, int b)
+        //{
+
+        //}
+        public void add(float a, float b)
+        {
+
+        }
+
+    }
+}
---
+++ Calculator/ConsoleApplication1/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Box
+    {
+        public double height;
+        public double width;
+        public Box()
+        {
+
+        }
+        public Box(double h, double w)
+        {
+            height = h;
+            width = w;
+        }
+        public void display()
+        {
+            Console.WriteLine("Height={0}\nWidth={1}\n", height, width);
+        }
+        public static Box operator +(Box b1, Box b2)
+        {
+            Box box = new Box();
+            box.height = b1.height + b2.height;
+            box.width = b1.width + b2.width;
+            return box;
+        }
+        public static Box operator -(Box b2, Box b1)
+        {
+            Box box = new Box();
+            box.height = b2.height - b1.height;
+            box.width = b2.width - b1.width;
+            return box;
+        }
+        public static bool operator <(Box b2, Box b1)
+        {
+            bool check;
+            if ((b1.height < b2.height) && (b1.width < b2.width))
+            {
+                check = true;
+            }
+            else
+            {
+                check = false;
+            }
+            return check;
+        }
+        public static bool operator >(Box b2, Box b1)
+        {
+            bool check;
+            if ((b1.height < b2.height) && (b1.width < b2.width))
+            {
+                check = true;
+            }
+            else
+            {
+                check = false;
+            }
+            return check;
+        }
+        public static Box operator ++(Box b)
+        {
+            Box box = new Box();
+            box.height = b.height + 1;
+            box.width = b.width + 1;
+            return box;
+        }
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
+
